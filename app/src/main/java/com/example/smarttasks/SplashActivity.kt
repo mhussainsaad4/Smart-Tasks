5c2a6510101f9db655f35359910703c134816573
mhussainsaad4 <mhussainsaad4@gmail.com>
2024-09-30 01:05:57 +0500

    splash done

--- app/src/main/java/com/example/smarttasks/SplashActivity.kt
+++ app/src/main/java/com/example/smarttasks/SplashActivity.kt
@@ -1,10 +1,17 @@
 package com.example.smarttasks
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.smarttasks.home.HomeActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +23,12 @@ class SplashActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        openHome()
+    }
+
+    private fun openHome() = CoroutineScope(Dispatchers.Main).launch {
+        delay(2000)
+        startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
     }
 }
